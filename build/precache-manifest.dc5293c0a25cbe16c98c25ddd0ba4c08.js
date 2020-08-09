0d77b9dbe6397d88401ac5b29ff94ee05b7f5afd
yavyletkov <yavyletkov@ya.ru>
2020-08-09 18:50:42 +0300

    .gitignore fix

---
+++ build/precache-manifest.dc5293c0a25cbe16c98c25ddd0ba4c08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed69539565e2384b02e95623107e579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c657f2db40f7b8b2550b",
+    "url": "/static/css/main.b8f6b337.chunk.css"
+  },
+  {
+    "revision": "f4716d9b5cc51bc7fec2",
+    "url": "/static/js/2.71820d7e.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.71820d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c657f2db40f7b8b2550b",
+    "url": "/static/js/main.c0c29709.chunk.js"
+  },
+  {
+    "revision": "62c97e2de70d3d4538ae",
+    "url": "/static/js/runtime-main.3dd29e43.js"
+  }
+]);
